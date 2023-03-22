54a1aa364dc3c0390a32630ae3cf0f5d04dfb0f8
Faithful <ahamefulefaithful@gmail.com>
2023-03-22 10:16:53 -0700

    1. I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function prints alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char m;
+
+	for (m = 'a' ; m >= 'z' ; m++)
+		_putchar(m);
+	_putchar('\n');
+}
+

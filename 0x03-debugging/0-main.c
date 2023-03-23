7a2da3777f3c77578dab1f7fdd00265030cf3c84
Faithful <ahamefulefaithful@gmail.com>
2023-03-23 10:08:59 -0700

    Multiple main debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - test function
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -9,8 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 0 ;
+	i = 0;
 	positive_or_negative(i);
 
-	return(0);
+	return (0);
 }

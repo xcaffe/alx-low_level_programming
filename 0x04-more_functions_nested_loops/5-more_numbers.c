c680b32f4231dee28da4940a33d658ccaf0d28c8
Faithful <ahamefulefaithful@gmail.com>
2023-03-24 22:51:53 -0700

    betty correction 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@ void more_numbers(void)
 {
 	int c, i;
 
-        for (i = 0; i <= 9; i++)
-        {
-	for (c = 0; c <=14; c++)
+	for (i = 0; i <= 9; i++)
 	{
-                if (c > 9)
-                _putchar((c / 10) + '0');
+	for (c = 0; c <= 14; c++)
+	{
+		if (c > 9)
+			_putchar((c / 10) + '0');
 		_putchar((c % 10) + '0');
-        }
-       	_putchar('\n');
+	}
+	_putchar('\n');
 	}
 }

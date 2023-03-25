d5383eaa310e3bcae69f87fcafa574780281ec1c
Faithful <ahamefulefaithful@gmail.com>
2023-03-24 22:43:12 -0700

    betty correction 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,14 @@
  */
 void print_most_numbers(void)
 {
-        int c;
+	int c;
 
-        for (c = 0; c <= 9; c++)
+	for (c = 0; c <= 9; c++)
 	{
 		if ((c == 2) || (c == 4))
 			continue;
 		else
-                _putchar(c + '0');
+			_putchar(c + '0');
 	}
-                _putchar('\n');
+	_putchar('\n');
 }

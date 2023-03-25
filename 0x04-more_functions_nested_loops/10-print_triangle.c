d8fb5bdf821a1760a2d7c8cbdddba5a13b450da0
Faithful <ahamefulefaithful@gmail.com>
2023-03-25 02:37:00 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - function print triangle
+ * @size: variable
+ * Return: none for now.
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+		for (b = size - a; b > 1; a--)
+		{
+		_putchar(32);
+		}
+		for (c = 0; c <= a; c++)
+		{
+			_putchar(32);
+		}
+		_putchar('\n');
+		}
+	}
+}

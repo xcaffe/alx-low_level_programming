ca7896d6006a8fe2234648771e5fd386fa37fca3
Faithful <ahamefulefaithful@gmail.com>
2023-03-24 20:59:31 -0700

    correct digit

--- 0x04-more_functions_nested_loops/3-print_number.c
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - function checks for a digit.
- * @c: input variable
- * Return: 1 or 0 should be the value.
+ * print_numbers - function prints number.
+ * Return: 0-9 should be the value.
  */
 void print_numbers(void)
 {
-        if (c >= 0 ; && c <= 9)
-        return (1);
-        else
-        return (0);
+	int c;
+
+        if (c = 0; c <= 9; c++)
+		_putchar(c + '0');
+		_putchar('\n');
 }

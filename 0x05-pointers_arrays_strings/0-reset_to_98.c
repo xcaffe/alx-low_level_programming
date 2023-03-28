a6a4fdde4f8f750612db864221dfb738d4330d78
Faithful <ahamefulefaithful@gmail.com>
2023-03-28 09:47:47 -0700

    betty correction 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
-/*
- * reset_to_98 - changes value of n to 98
+/**
+ * reset_to_98 - function changes value of n to 98
  * @n: pointer variable
- * Return: value is 0
+ * Return: 0
  */
 void reset_to_98(int *n)
 {

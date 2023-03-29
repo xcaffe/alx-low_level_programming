d8aacf1e70bc11c0e5dd8b8e2f1a248534a7474b
Faithful <ahamefulefaithful@gmail.com>
2023-03-29 11:09:58 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * _strncat - concatenate two strings
- * @dest: parameter
- * @src: parameter
- * @n: parameter
+ * @dest: string parameter to be appended
+ * @src: string parameter that would append to dest
+ * @n: parameter for number of bytes
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	size_t dest_len = strlen(dest);
-	size_t i;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + 1] = src[i];
-	dest[des_len + i] = '\0';
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }

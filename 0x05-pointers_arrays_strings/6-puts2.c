a00ca1153549584facde760da68dbf033acdba92
Faithful <ahamefulefaithful@gmail.com>
2023-03-28 21:01:33 -0700

    prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print string atarting with the first
+ * @str: parameter
+ * Return: 0;
+ */
+void puts2(char *str)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

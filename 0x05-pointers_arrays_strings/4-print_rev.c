5cdadeb0980c572b6bd1dafe73db86da17430724
Faithful <ahamefulefaithful@gmail.com>
2023-03-28 18:35:27 -0700

    question 4 correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int i;
-	int count = 0;
+        int i = 0;
+        int count;
 
-	for (i = 0; s[i] != '\0'; i++)
-		count++;
-	for (i = count; i >= 0; i--)
-		_putchar(s[i]);
-	_putchar('\n');
+        for (i = 0; s[i] != '\0'; i++)
+                count++;
+        for (i = count; i >= 0; i--)
+                _putchar(s[i]);
+        _putchar('\n');
 }
-

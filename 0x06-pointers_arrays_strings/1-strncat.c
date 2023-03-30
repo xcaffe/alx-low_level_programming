33540658cea3a4d51aaaf6686b812731dd027354
Faithful <ahamefulefaithful@gmail.com>
2023-03-30 09:01:50 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + 1] = src[i];
+		dest[dest_len++] = src[i];
 	dest[dest_len + i] = '\0';
 
 	return (dest);

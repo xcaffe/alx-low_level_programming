b92a3648e6bec41c35efced0c23009168a6909ed
Faithful <ahamefulefaithful@gmail.com>
2023-04-04 21:15:16 -0700

    sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer value
+ * @to: char
+ * Return: no value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

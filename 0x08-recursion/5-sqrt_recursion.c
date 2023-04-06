e24ce9f9b1d1d0d2f267b036631be826664b1b74
Faithful <ahamefulefaithful@gmail.com>
2023-04-06 13:06:06 -0700

    natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+int actual_sqrt_recursion(int n, int i);
+/**
+ * _sqrt_recursion -  natural square root of a number
+ * @n: the number for the natural square root
+ * Return: value -1 or 1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+/**
+ * actual_sqrt_recursion -recurses to find natural square root of a number
+ * @n: the number for the natural square root
+ * @i: iterator
+ * Return: result of the square
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}

f6c9fdbbcec3de64bfe6e46fbffbd39c91c785cc
Faithful <ahamefulefaithful@gmail.com>
2023-04-17 21:26:35 -0700

    defines a macro

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,37 +10,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-	while (s2[k])
+	char *s;
+	while (s1 && s1[k])
 		k++;
-	if (n >= k)
-		l = i + k;
+	while (s2 && s2[l])
+		l++;
+	if (n < l)
+		s = malloc(sizeof(char) * (k + n + 1));
 	else
-		l = i + n;
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
+		s = malloc(sizeof(char) * (k + l + 1));
+	if (!s2)
 		return (NULL);
-	k = 0;
-	while (j < l)
+	while (i < k)
 	{
-		if (j <= i)
-			str[j] = s1[j];
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+		s[i] = s1[i];
+		i++;
 	}
-	str[j] = '\0';
-	return (str);
+	while (n < l && i < (k + n))
+		s[i++] = s2[j++];
+	while (n >= l && i (k + l))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0. Multiple mains
+0x0D. C - Preprocessor

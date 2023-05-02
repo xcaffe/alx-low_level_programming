9fe2402b6194d6d2fe06234b9a005282c88b9741
xcaffe <ahamefulefaithful@gmail.com>
2023-05-02 00:27:15 -0700

    betty 4-free_grid correction

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,11 +9,11 @@
  */
 void free_grid(int **grid, int height)
 {
-        int a;
+	int a;
 
-        for (a = 0; a < height; a++)
-        {
-                free(grid[a]);
-        }
-        free(grid);
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
 }

49b7f4a4c654c35f4aae18a9c7ce3674328560a0
xcaffe <ahamefulefaithful@gmail.com>
2023-07-10 11:41:46 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads and prints number of letters in POSIX standard output
+ * @filename: text file to be read from
+ * @letters: numbers of letters it should read and print
+ * Return: 0 if file name is 0 and if write fails or does not write the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *bay;
+	ssize_t a, b, c;
+
+	a = open(filename, O_RDONLY);
+	if (a == -1)
+		return (0);
+	bay = malloc(sizeof(char) * letters);
+	c = read(a, bay, letters);
+	b = write(STDOUT_FILENO, bay, c);
+
+	free(bay);
+	close(a);
+	return (b);
+}

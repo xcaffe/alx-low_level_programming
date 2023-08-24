5d05eca0333aa819f13b94c4ee1ecffeb11aefac
xcaffe <ahamefulefaithful@gmail.com>
2023-08-24 04:46:38 -0700

    correction no6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,23 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int fich = 0;
+	int a = 0;
+	char *r = str;
+	int b;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*r != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		r++;
+		fich++;
+	}
+	a = fich - 1;
+	for (b = 0 ; b <= a ; b++)
+	{
+		if (b % 2 == 0)
+		{
+			_putchar(str[b]);
+		}
 	}
 	_putchar('\n');
 }

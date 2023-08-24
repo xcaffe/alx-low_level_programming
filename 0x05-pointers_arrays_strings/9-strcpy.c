1536c49a732b5cd544ed8437003f1d37ac04b455
xcaffe <ahamefulefaithful@gmail.com>
2023-08-24 03:13:07 -0700

    no 9 and 10

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
+	int x = 0;
 
-	if (dest == Null)
-		return (Null);
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for ( ; x < i ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
 	return (dest);
 }
